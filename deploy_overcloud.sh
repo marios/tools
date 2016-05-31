89ec12a3b995af351d5c893dc61c761841f4af90
marios <marios@redhat.com>
2016-05-31 10:25:16 +0300

    wow

--- deploy_overcloud.sh
+++ deploy_overcloud.sh
@@ -1,21 +1,15 @@
 #!/bin/bash
 
-sudo ip route add 10.0.0.0/24 via 192.0.2.1 dev br-ctlplane || true
+#add some swap
+if [[ -z $(cat /proc/swaps | grep -v Filename) ]]; then
+  sudo dd if=/dev/zero of=/swapfile count=4k bs=1M
+  sudo mkswap /swapfile
+  sudo chmod 0600 /swapfile
+  sudo swapon /swapfile
+  cat /proc/swaps
+fi
 
-sudo sed -i 's/^#num_engine_workers.*$/num_engine_workers = 4/g' /etc/heat/heat.conf
-
-sudo systemctl restart openstack-heat-engine
-
-echo "   DnsServers: [\"192.168.122.1\"]" >> network_env.yaml
-
-echo "add some swap to undercloud"
-sudo dd if=/dev/zero of=/swapfile count=4k bs=1M
-sudo mkswap /swapfile
-sudo chmod 0600 /swapfile
-sudo swapon /swapfile
-cat /proc/swaps
-
-deploy_cmd="openstack overcloud deploy --templates --control-scale 3 --compute-scale 1 --libvirt-type qemu -e /usr/share/openstack-tripleo-heat-templates/environments/network-isolation.yaml -e /usr/share/openstack-tripleo-heat-templates/environments/net-single-nic-with-vlans.yaml -e network_env.yaml --ntp-server '0.fedora.pool.ntp.org'"
+deploy_cmd="openstack overcloud deploy --templates /home/stack/tripleo-heat-templates -e /home/stack/tripleo-heat-templates/overcloud-resource-registry-puppet.yaml -e /home/stack/tripleo-heat-templates/environments/puppet-pacemaker.yaml  --control-scale 3 --compute-scale 1 --libvirt-type qemu -e /home/stack/tripleo-heat-templates/environments/network-isolation.yaml -e /home/stack/tripleo-heat-templates/environments/net-single-nic-with-vlans.yaml -e network_env.yaml --ntp-server '0.fedora.pool.ntp.org'"
 
 echo "going to deploy like this $deploy_cmd"
 echo "network env like"
