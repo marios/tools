92179e348ffa9c6e73571ab4cbc06403981ab7ed
marios <marios@redhat.com>
2016-06-15 13:52:45 +0300

    woo parameterize tht dir

--- deploy_overcloud.sh
+++ deploy_overcloud.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+THT_ROOT=/usr/share/openstack-tripleo-heat-templates
+#THT_ROOT=/home/stack/tripleo-heat-templates/
+
 #add some swap
 if [[ -z $(cat /proc/swaps | grep -v Filename) ]]; then
   sudo dd if=/dev/zero of=/swapfile count=4k bs=1M
@@ -9,7 +12,7 @@ if [[ -z $(cat /proc/swaps | grep -v Filename) ]]; then
   cat /proc/swaps
 fi
 
-deploy_cmd="openstack overcloud deploy --templates /home/stack/tripleo-heat-templates -e /home/stack/tripleo-heat-templates/overcloud-resource-registry-puppet.yaml -e /home/stack/tripleo-heat-templates/environments/puppet-pacemaker.yaml  --control-scale 3 --compute-scale 1 --libvirt-type qemu -e /home/stack/tripleo-heat-templates/environments/network-isolation.yaml -e /home/stack/tripleo-heat-templates/environments/net-single-nic-with-vlans.yaml -e network_env.yaml --ntp-server '0.fedora.pool.ntp.org'"
+deploy_cmd="openstack overcloud deploy --templates $THT_ROOT -e $THT_ROOT/overcloud-resource-registry-puppet.yaml -e $THT_ROOT/environments/puppet-pacemaker.yaml  --control-scale 3 --compute-scale 1 --libvirt-type qemu -e $THT_ROOT/environments/network-isolation.yaml -e $THT_ROOT/environments/net-single-nic-with-vlans.yaml -e network_env.yaml --ntp-server '0.fedora.pool.ntp.org'"
 
 echo "going to deploy like this $deploy_cmd"
 echo "network env like"
@@ -23,3 +26,4 @@ echo "started $deploy_cmd at `date`" > ~/deploy_time
 source ~/stackrc
 $deploy_cmd
 echo "OK DONE at  `date`" >> ~/deploy_time
+
